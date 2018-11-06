72a806de9642ac46b9df86bd555e00d430943071
Varun <i.nurav@gmail.com>
2018-11-06 15:40:19 -0800

    User input

--- gossip.go
+++ gossip.go
@@ -2,12 +2,20 @@ package main;
 
 import (
 	"fmt"
+	"os"
 	"golang.org/x/sync/semaphore"
+	"strconv"
 )
 
 const numNodes = 8;
 
 var p_sem = semaphore.NewWeighted(1);
+
+var hb_counter_T int64;
+var hb_send_T int64;
+var node_fail_T int64;
+var err error;
+var shutdown_T int64;
 
 type heartbit struct {
 	id int
@@ -15,11 +23,35 @@ type heartbit struct {
 	time uint64
 }
 
-
 func main() {
+	if(len(os.Args) != 4){
+		fmt.Println("Usage: ./gossip_go <hb counter T> <hb send T> <node fail T>");
+		os.Exit(1);
+	}
+
+	hb_counter_T, err = strconv.ParseInt(os.Args[1], 10, 64);
+	if err != nil {
+		fmt.Println(err);
+		os.Exit(1);
+	}
+	hb_send_T, err = strconv.ParseInt(os.Args[2], 10, 64);
+	if err != nil {
+		fmt.Println(err);
+		os.Exit(1);
+	}
+	node_fail_T, err = strconv.ParseInt(os.Args[3], 10, 64);
+	if err != nil {
+		fmt.Println(err);
+		os.Exit(1);
+	}
+
+	shutdown_T = 30;
+
+	fmt.Println(hb_counter_T, " ", hb_send_T, " ", node_fail_T);
+
 	var gossip_channels [numNodes]chan heartbit;
 	var print_channel = make(chan heartbit, numNodes+1);
-	for i := range gossip_channels {
+	for i := range gossip_channels { 
 		gossip_channels[i] = make(chan heartbit);
 	}
 
@@ -58,7 +90,9 @@ func spinUpNode(id int, neighbors []int, channel [numNodes]chan heartbit, p_chan
 	my_hb.hb = 0;
 	my_hb.time = 0;
 	
-	local_t := 0;
+	var local_t uint64;
+	local_t = 0;
+
 	for {
 		if local_t == 10  {
 			p_channel <- my_hb
